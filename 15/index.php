0015c42c4575172536ea107e5e8a31053dc6478d
Yura Holudeev <yustasvega@users.noreply.github.com>
2023-04-16 00:16:08 +0700

    15 print

---
+++ 15/index.php
@@ -0,0 +1,59 @@
+<?php
+error_reporting(error_level: -1);
+//Цикл while
+
+/* $i = 1;
+
+while ($i < 21) {
+   echo "{$i}<br>";
+   $i++;
+}
+ */
+
+
+/* echo '<table border="1" width="100%"';
+
+$tr = 1;
+while ($tr <= 10) {
+   echo "<tr>";
+   $td = 1;
+   while ($td <= 20) {
+      echo "<td>" . $td * $tr . "</td>";
+      $td++;
+   }
+   echo "</tr>";
+   $tr++;
+}
+
+echo '</table>'; */
+
+/* $i = 1;
+
+while ($i < 10) {
+   echo "{$i}<br>";
+   $i++;
+} */
+
+// do-while
+/* $i = 1;
+
+do {
+   echo "{$i}<br>";
+   $i++;
+} while ($i <= 10); */
+
+// $i = 1;
+// $r = 7;
+// do {
+//    if ($i < 5) {
+//       echo "i ещё недостаточно велико";
+//       break;
+//    }
+//    $i *= $r;
+//    if ($i < $r) {
+//       break;
+//    }
+//    echo "значение i уже подходит";
+
+//    /* обработка i */
+// } while (0);
