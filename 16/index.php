7d840187c8a8fcabc59bfbb8a42fdeaa2b5e7d9b
Yura Holudeev <yustasvega@users.noreply.github.com>
2023-04-16 00:56:18 +0700

    16 Задание

---
+++ 16/index.php
@@ -0,0 +1,33 @@
+<?php
+error_reporting(error_level: -1);
+//Цикл while//Задание
+
+
+/* 1. Используйя цикл while, реализуйте вывод выпадающего списка select с годами от 1980  до 2023 включительно. */
+
+/* $year = 1980;
+
+echo "<select>";
+while ($year <= 2024) {
+   echo "<option value='{$year}'>{$year}</option>";
+   $year++;
+}
+echo "</select>"; */
+
+/* 2. Используйя цикл while, реализуйте таблицу умножения. */
+
+/* echo '<table border="1" width="100%"';
+
+$tr = 1;
+while ($tr <= 9) {
+   echo "<tr>";
+   $td = 1;
+   while ($td <= 9) {
+      echo "<td>$td * $tr = " . $td * $tr . "</td>";
+      $td++;
+   }
+   echo "</tr>";
+   $tr++;
+}
+
+echo '</table>'; */
