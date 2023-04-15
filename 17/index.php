06db61a80ea3cfaec7deda70bcb34c5ae1444594
Yura Holudeev <yustasvega@users.noreply.github.com>
2023-04-16 01:48:34 +0700

    for17

---
+++ 17/index.php
@@ -0,0 +1,44 @@
+<?php
+error_reporting(error_level: -1);
+//Цикл for
+
+/* $i = 1;
+
+while ($i < 10) {
+   echo "{$i}<br>";
+   $i++;
+} */
+
+/* for ($i = 1; $i < 10; $i++) {
+   echo "{$i}<br>";
+}
+ */
+
+/* for ($i = 20; $i >= 1; $i--) {
+   echo "{$i}<br>";
+} */
+
+/* $year = 1980;
+
+echo "<select>";
+while ($year <= 2024) {
+   echo "<option value='{$year}'>{$year}</option>";
+   $year++;
+}
+echo "</select>"; */
+
+/* echo "<select>";
+for ($year = 1980; $year <=  2024; $year++) {
+   echo "<option value='{$year}'>{$year}</option>";
+}
+echo "</select>"; */
+
+/* echo '<table border="1" width="100%"';
+for ($tr = 1; $tr < 10; $tr++) {
+   echo "<tr>";
+   for ($td = 1; $td < 10; $td++) {
+      echo "<td>$td * $tr = " . $td * $tr . "</td>";
+   }
+   echo "</tr>";
+}
+echo '</table>'; */
---
+++ 18/index.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(error_level: -1);
+
+// Оператор switch:
+
+/* $i = 4;
+
+switch ($i) {
+   case 0:
+      echo "i равно 0";
+      break;
+   case 1:
+      echo "i равно 1";
+      break;
+   case 2:
+      echo "i равно 2";
+      break;
+   case 3:
+      echo "i равно 3";
+      break;
+   case 4:
+      echo "i равно 4";
+      break;
+   default:
+      echo 5;
+} */
