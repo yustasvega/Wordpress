60b1db4421b6a36f38afd0a31a6606b4c4bb30dd
Yura Holudeev <yustasvega@users.noreply.github.com>
2023-04-19 06:06:45 +0700

    Custom functions 1

---
+++ 24/index.php
@@ -0,0 +1,50 @@
+<?php
+error_reporting(error_level: -1);
+
+//function/Custom functions 1
+
+
+/* test();
+function test(): void
+{
+   echo '<p>Hello, world!</p>';
+}
+
+test();
+test(); */
+
+/* 
+function hello($name = 'Guest')
+{
+   echo "<p>Hello, {$name}</p>";
+}
+
+hello('Вася!'); */
+
+/* function sum($a, $b, $c = 2,)
+{
+   echo ($a + $b) * $c;
+}
+
+sum(1, 2, 3); */
+
+/* $a = 5;
+function test($a)
+{
+   global $a;
+   $a += 10;
+   var_dump($a);
+}
+
+var_dump($a);
+test($a);
+var_dump($a);
+ */
+
+/* function test(&$num)
+{
+   $num += 10;
+}
+$num = 20;
+test($num);
+var_dump($num); */
